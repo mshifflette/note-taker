322856258d31f05120571f69e2ba09953e9f7535
Gokuchan <73154359+mshifflette@users.noreply.github.com>
2021-10-21 18:21:25 -0700

    working the server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const notes = require('./db/db.json')
+var uniqid = require('uniqid'); 
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, 'public/notes.html'))
+); 
+
+app.get('/api/notes', (req, res) =>
+res.json(notes)); 
+
+app.post('/api/notes', ((req, res)=>{
+notes.push(req.body); 
+}));
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+); 
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
